e829d69d5aadf5e086c103bf09793043bc231122
Sara Romao <sara-romao@live.com>
2025-08-24 13:22:31 -0300

    added theme in element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
-import './globals.css'
+'use client'
+import { Header } from './_component/heander';
+import './globals.css';
+import { useState } from 'react';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [theme, setTheme] = useState(false)
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" data-theme={!theme ? "dark":""}>
+      <body>
+        <div className="bg-white dark:bg-black">
+          <Header setTheme={setTheme} isDark={theme} />
+          {children}
+        </div>
+      </body>
     </html>
   )
 }

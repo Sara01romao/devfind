557ddc649020403ce2039b12b61496b2e1612973
Sara Romao <sara-romao@live.com>
2025-08-24 16:42:19 -0300

    added font space mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { Header } from './_component/heander';
 import './globals.css';
 import { useState } from 'react';
 
+import { Space_Mono } from 'next/font/google'
+ 
+const space_mono = Space_Mono({
+  subsets: ['latin'],
+  weight:['400', '700']
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -10,12 +17,10 @@ export default function RootLayout({
 }) {
   const [theme, setTheme] = useState(false)
   return (
-    <html lang="en" data-theme={!theme ? "dark":""}>
-      <body>
-        <div className="bg-white dark:bg-black">
-          <Header setTheme={setTheme} isDark={theme} />
-          {children}
-        </div>
+    <html lang="en" data-theme={theme ? "light":"dark"} className={space_mono.className}>
+      <body className="bg-[#F6F8FF] dark:bg-[#141D2F]">
+        <Header setTheme={setTheme} isDark={theme} />
+        {children}
       </body>
     </html>
   )
